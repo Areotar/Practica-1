8749745baa6d95badd738576b42e0aeeb8911c50
Areotar <alexreotar2005@gmail.com>
2024-11-06 17:36:43 +0100

    Programa completo.

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+// import EligeBoton from './components/EligeBoton';
+// import Saludo from './components/saludo';
+// import Suma from './components/suma';
+// import Mensaje from './components/Mensaje';
+import PeopleList from './components/PeopleList';
+
+function App() {
+  return (
+    <div className="App">
+      <PeopleList/>
+    </div>
+  );
+}
+export default App;
